3d680b2ff62587b780d4052879cbe32d6e78c75a
LiCanhui-Huyi <1509702529@qq.com>
2023-01-16 21:00:35 +0800

    R pull test

--- R/hello.R
+++ R/hello.R
@@ -14,6 +14,6 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 hello <- function() {
-  print("Hello, world!")
-  print("hello github!")
+  print("hello Iself!")
+  print("hello rstudio!")
 }
